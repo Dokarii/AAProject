e5168f142df02633e5826f414718b4c471ab7e63
Dokarii <ricardotejedor18@gmail.com>
2025-05-28 14:55:12 -0500

    Se crean todos los paquetes.

---
+++ src/main/java/com/example/AskAcademy/AskAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.example.AskAcademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AskAcademy/AskAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AskAcademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
